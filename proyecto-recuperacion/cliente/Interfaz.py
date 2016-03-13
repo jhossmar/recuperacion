8299f9da1734e551144865cedb65121feaecc748
jhossmar <jhosmar_ramos@hotmail.com>
2016-03-13 09:18:43 -0400

    clases

---
+++ proyecto-recuperacion/cliente/Interfaz.py
@@ -0,0 +1,17 @@
+'''
+Created on 13 de mar. de 2016
+
+@author: marcelo
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ proyecto-recuperacion/server/Indexador.py
@@ -0,0 +1,17 @@
+'''
+Created on 13 de mar. de 2016
+
+@author: marcelo
+'''
+
+class Indexador(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
