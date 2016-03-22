d8db88e386db0c2a6f1578ce46c5a22d23e5df1f
haroldbuhezo <harolbueso@gmail.com>
2016-03-22 09:19:24 -0400

    Revert "hilo socket-cliente"
    This reverts commit aa4370e62f211f76dc4c110d94af6dd6352428df.

--- proyecto-recuperacion/cliente/Interfaz.py
+++ proyecto-recuperacion/cliente/Interfaz.py
@@ -4,9 +4,7 @@ Created on 13 de mar. de 2016
 @author: marcelo
 '''
 from Tkinter import *
-from Socket import Socket
 class Interfaz(object):
-
     '''
     classdocs
     '''
@@ -15,7 +13,6 @@ class Interfaz(object):
     def __init__(self):
         print "iniciando Intefaz"
         self.c = Tk()
-        self.txt_consulta = Entry(self.c)
         self.c.title("RECUPERCION DE LA INFORMACION")
         
         ###configuraciones Ventana
@@ -24,28 +21,17 @@ class Interfaz(object):
         
         ## elementos 
         labe4 = Label(self.c,text="SISTEMA DE BUSQUEDA" )
-        
-        btn_enviar = Button(self.c, text="ENVIAR",command=lambda:self.enviar(Entry.get(self.txt_consulta))) ## poner un command=<nombreFuncion>
+        txt_consulta = Entry(self.c)
+        btn_enviar = Button(self.c, text="ENVIAR") ## poner un command=<nombreFuncion>
         
         ##cargando elementos 
         labe4.pack()
-        self.txt_consulta.pack()
+        txt_consulta.pack()
         btn_enviar.pack()
         
         
     def mostrar(self):
         
         self.c.mainloop()
-    
-    def enviar(self,consulta):
-        #print consulta
-        listaServidores = ['109876543','192.168.43.5','192.168.3.2']
-        listaSocketEsperando = []
-        for servidor in listaServidores:
-            sk = Socket(servidor,consulta)
-            sk.start()
-            #listaSocketEsperando.append(sk)
-            print "next IP"
-            
        
         
--- proyecto-recuperacion/cliente/Socket.py
+++ proyecto-recuperacion/cliente/Socket.py
@@ -5,34 +5,20 @@ Created on 15 de mar. de 2016
 '''
 
 import socket
-import threading
 
-class Socket(threading.Thread):
+class Socket(object):
 
-    def __init__(self, host,consulta):
-       threading.Thread.__init__(self)
-       print "iniciando socket cliente:"
+    def __init__(self, host):
+       print "iniciando socket servidor:"
        self.s = socket.socket()
-       self.host = host
-       self.consulta = consulta
-     
-    
-    def run(self):
-        try:
-           
-            self.s.connect((self.host,8087))
-            self.enviarDatos(self.consulta)
-#             while 1:
-#                 print "esperando respuesta del servidor"
-        except Exception,e:
-            print 'no se conecto con el servidor',self.host
-       
+       self.s.connect((host,8087))
+
        
         
     
     def enviarDatos(self, string):
         print "enviando datos:"
-        mensaje = string ##REmplazar por la consulta del usuario
+        mensaje = raw_input("> ") ##REmplazar por la consulta del usuario
         self.s.send(mensaje)
        
      
